3441d3c291acf36ef298af7069c805df840a6a5d
juancoutinhoflooxmongagua <juan.coutinho.flooxmongagua@gmail.com>
2025-04-09 11:35:01 -0300

    feat: fix bugs | guess error

--- src/api/billingCycle/billingCycleService.js
+++ src/api/billingCycle/billingCycleService.js
@@ -1,6 +1,5 @@
 const express = require('express')
 const BillingCycle = require('./billingCycle')
-const errorHandler = require('../../common/errorHandler')
 
 const router = express.Router()
 
@@ -9,8 +8,7 @@ router.get('/billingCycles', async (req, res, next) => {
     const data = await BillingCycle.find()
     res.json(data)
   } catch (err) {
-    res.locals.bundle = { errors: [err] }
-    next()
+    next(err)
   }
 })
 
@@ -20,8 +18,7 @@ router.post('/billingCycles', async (req, res, next) => {
     const saved = await billing.save()
     res.json(saved)
   } catch (err) {
-    res.locals.bundle = { errors: [err] }
-    next()
+    next(err)
   }
 })
 
@@ -30,28 +27,27 @@ router.get('/billingCycles/count', async (req, res, next) => {
     const count = await BillingCycle.countDocuments()
     res.json({ value: count })
   } catch (err) {
-    res.locals.bundle = { errors: [err] }
-    next()
+    next(err)
   }
 })
 
 router.get('/billingCycles/summary', async (req, res, next) => {
   try {
     const result = await BillingCycle.aggregate([
-      { 
+      {
         $project: {
           credit: { $sum: "$credits.value" },
           debt: { $sum: "$debts.value" }
         }
       },
-      { 
+      {
         $group: {
           _id: null,
           credit: { $sum: "$credit" },
           debt: { $sum: "$debt" }
         }
       },
-      { 
+      {
         $project: {
           _id: 0,
           credit: 1,
@@ -61,8 +57,7 @@ router.get('/billingCycles/summary', async (req, res, next) => {
     ])
     res.json(result[0] || { credit: 0, debt: 0 })
   } catch (err) {
-    res.locals.bundle = { errors: [err] }
-    next()
+    next(err)
   }
 })
 
--- src/common/errorHandler.js
+++ src/common/errorHandler.js
@@ -1,19 +1,5 @@
-const _ = require('lodash')
-
-const parseErrors = (nodeRestfulErrors) => {
-  const errors = []
-  _.forIn(nodeRestfulErrors, error => errors.push(error.message))
-  return errors
-}
-
-module.exports = (req, res) => {
-    const bundle = res.locals.bundle
-  
-    if (bundle?.errors?.length > 0) {
-      const errors = bundle.errors.map(err => err.message ?? err)
-      res.status(500).json({ errors })
-    } else {
-      res.status(500).json({ errors: ['Erro desconhecido.'] })
-    }
+module.exports = (err, req, res, next) => {
+    const message = err.message || 'Erro desconhecido.'
+    res.status(500).json({ errors: [message] })
   }
   
