822f03c74f161d432359728d3b70c2eedb51ed98
juancoutinhoflooxmongagua <juan.coutinho.flooxmongagua@gmail.com>
2025-04-09 11:18:54 -0300

    feat: method count

--- src/api/billingCycle/billingCycleService.js
+++ src/api/billingCycle/billingCycleService.js
@@ -1,17 +1,33 @@
-// billingCycleService.js
 const express = require('express')
 const BillingCycle = require('./billingCycle')
 const router = express.Router()
 
 router.get('/billingCycles', async (req, res) => {
-  const data = await BillingCycle.find()
-  res.json(data)
+  try {
+    const data = await BillingCycle.find()
+    res.json(data)
+  } catch (err) {
+    res.status(500).json({ errors: [err] })
+  }
 })
 
 router.post('/billingCycles', async (req, res) => {
-  const billing = new BillingCycle(req.body)
-  const saved = await billing.save()
-  res.json(saved)
+  try {
+    const billing = new BillingCycle(req.body)
+    const saved = await billing.save()
+    res.json(saved)
+  } catch (err) {
+    res.status(500).json({ errors: [err] })
+  }
+})
+
+router.get('/billingCycles/count', async (req, res) => {
+  try {
+    const count = await BillingCycle.countDocuments()
+    res.json({ value: count })
+  } catch (err) {
+    res.status(500).json({ errors: [err] })
+  }
 })
 
 module.exports = router
